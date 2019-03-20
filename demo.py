5837f5a038c9e74daebfa7194e250e5ae892dcf4
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2019-03-20 16:08:35 +0100

    Add custom name

--- demo.py
+++ demo.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return "Hello zappa!", 200
 
+@app.route('/hello/<name>')
+@app.route('/hello/')
+def custom_hello(name=None):
+    if name is None:
+        return "Tell me your name", 200
+    else:
+        return "Hello " + name + "!", 200
+
 # We only need this for local development.
 if __name__ == '__main__':
     app.run()
